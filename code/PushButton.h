8adc0a23f1974409013b41057d25564fac77d90e
Peter Nagy <peternagy96@gmail.com>
2020-01-28 15:31:54 +0100

    remove continuous longpress function

--- code/PushButton.h
+++ code/PushButton.h
@@ -5,21 +5,21 @@
 #ifndef __PUSHBUTTON_H
 #define __PUSHBUTTON_H
 
-class PushButtonClass {
-  public:
-    void press (void);
-    void release (void);
-    bool rising (void);
-    bool falling (void);
-    bool fallingLongPress (void);
-    bool fallingContinuous (void);
-    bool longPress (void); 
-    bool longPressContinuous (void); 
+class PushButtonClass
+{
+public:
+    void press(void);
+    void release(void);
+    bool rising(void);
+    bool falling(void);
+    bool fallingLongPress(void);
+    bool fallingContinuous(void);
+    bool longPress(void);
 
     bool pressed = false;
     bool wasPressed = false;
-    
-  private:
+
+private:
     bool longPressed = false;
     bool wasLongPressed = false;
     uint32_t longPressTs = 0;
