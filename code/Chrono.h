fb9f68af94eb7efcae48b7514b2d911ef46de955
Peter Nagy <peternagy96@gmail.com>
2020-01-29 07:59:47 +0100

    add abstract chrono class

--- code/Chrono.h
+++ code/Chrono.h
@@ -1,31 +1,26 @@
 /*
-* Chrono class to keep track of time
+* Abstract class with basic time-keeping capabilities
 */
 
 #ifndef __CHRONO_H
 #define __CHRONO_H
 
-/*
- * Class for performing simple timekeeping operations
- * can be used for a chronometer or countdown timer
- */
 class ChronoClass {
-  public:
-    void increment10th (void) volatile;
-    void increment10sec (void) volatile;
-    bool decrement10sec (void) volatile;
-    void incrementMin (void) volatile;
-    bool decrementMin (void) volatile;
-    void incrementSec (void) volatile;
-    bool decrementSec (void) volatile;
-    void reset (void) volatile;
-    void copy (volatile ChronoClass *) volatile;
-    void roundup (void) volatile;
+   public:
+    void incrementDigit1(void) volatile = 0;
+    bool decrementDigit1(void) volatile = 0;
+    void incrementDigit2(void) volatile = 0;
+    bool decrementDigit2(void) volatile = 0;
+    void incrementDigit3(void) volatile = 0;
+    bool decrementDigit3(void) volatile = 0;
+    void incrementDigit4(void) volatile = 0;
+    bool decrementDigit4(void) volatile = 0;
 
-    volatile int8_t tenth = 0;
+    void reset(void) volatile = 0;
+    void copy(volatile ChronoClass *) volatile = 0;
+    void roundup(void) volatile = 0;
+
     volatile int8_t second = 0;
     volatile int8_t minute = 0;
     volatile int8_t hour = 0;
-    
-  private:
 };
