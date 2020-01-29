2fa4b5c5bbc7a770699550b87f2c69d9fcb80d4d
Peter Nagy <peternagy96@gmail.com>
2020-01-29 09:20:36 +0100

    added inheritance from ChronoClass

--- code/Timer.cpp
+++ code/Timer.cpp
@@ -4,119 +4,56 @@
 
 #include "Timer.h"
 
-void TimerClass::initialize (void (*callback)(bool)) {
-  this->callback = callback;
-  defaultTm.tenth = 0;
-  defaultTm.second = 0;
-  defaultTm.minute = 5;
-  defaultTm.hour = 0;
-  reset ();
+void TimerClass::initialize(void (*callback)(bool)) {
+    defaultTm.second = 0;
+    defaultTm.minute = 4;
+    reset();
 }
 
-void TimerClass::loopHandler (void) {
-  uint32_t ts = millis ();
-  
-  if (tickFlag) { 
-    if (!alarm) {
-      alarm = tm.decrementSec (); 
-      if (alarm) {
-        //stop ();
-        alarmTs = ts;
-        tm.copy (&defaultTm);
-        Nixie.resetBlinking ();
-        Nixie.blinkAll (true);
-        Buzzer.playMelody2 ();
-      }
+void TimerClass::incrementRight(void) {
+    tm.incrementRight();
+}
+
+void TimerClass::decrementRight(void) {
+    tm.decrementRight();
+    if (running && alarmGoesOff()) {
+        alarm = true
     }
-    else {
-      tm.incrementSec ();
+}
+
+void alarmGoesOff(void) {
+    if (tm.minute == 0 && tm.hour == 0) {
+        alarm = true
     }
-    displayRefresh ();
-    tickFlag = false;
-  }
-  
-  if (alarm && ts - alarmTs > TIMER_ALARM_DURATION) resetAlarm ();
 }
 
-void TimerClass::tick (void) {
-  if (running) {  
-    tickFlag = true;
-  }
+void TimerClass::incrementLeft(void) {
+    tm.incrementLeft();
 }
 
-void TimerClass::secondIncrease (void) {
-  stop ();
-  tm.increment10sec ();
-  displayRefresh ();
-  start ();
+void TimerClass::decrementLeft(void) {
+    tm.decrementLeft();
 }
 
-void TimerClass::secondDecrease (void) {
-  bool rv;
-  stop ();
-  rv = tm.decrement10sec ();
-  displayRefresh ();
-  if (rv) stop ();
-  else start ();
+void TimerClass::start(void) {
+    defaultTm.copy(&tm);
+    active = true;
 }
 
-void TimerClass::minuteIncrease (void) {
-  stop ();
-  tm.incrementMin ();
-  displayRefresh ();
-  start ();
-}
-
-void TimerClass::minuteDecrease (void) {
-  bool rv;
-  stop ();
-  rv = tm.decrementMin ();
-  displayRefresh ();
-  if (rv) stop ();
-  else start ();
-}
-
-void TimerClass::displayRefresh (void) {
-  digits.value[0] = dec2bcdLow  (tm.second);
-  digits.value[1] = dec2bcdHigh (tm.second);
-  digits.value[2] = dec2bcdLow  (tm.minute);
-  digits.value[3] = dec2bcdHigh (tm.minute);
-  digits.value[4] = dec2bcdLow  (tm.hour);
-  digits.value[5] = dec2bcdHigh (tm.hour); 
-}
-
-void TimerClass::start (void) {
-  defaultTm.copy (&tm);
-  active = true;
-  if (!running) {
-    running = true;
-    callback (true);
-  }  
-}
-
-void TimerClass::stop (void) {
-  if (running) {
+void TimerClass::stop(void) {
     running = false;
-    callback (false);    
-  }
 }
 
-void TimerClass::resetAlarm (void) {
-  if (alarm) {
-    alarm = false;
-    Nixie.blinkAll (false);
-    Buzzer.stop ();
-    stop ();
-  }
+void TimerClass::resetAlarm(void) {
+    if (alarm) {
+        alarm = false;
+        stop();
+    }
 }
 
-void TimerClass::reset (void) {
-  resetAlarm ();
-  active = false;
-  running = false;
-  Nixie.resetDigits (&digits);
-  callback (false);
-  defaultTm.roundup ();
-  tm.copy (&defaultTm);
-  displayRefresh ();
+void TimerClass::reset(void) {
+    resetAlarm();
+    running = false;
+    defaultTm.roundup();
+    tm.copy(&defaultTm);
 }
--- code/Timer.h
+++ code/Timer.h
@@ -5,30 +5,24 @@
 #ifndef __TIMER_H
 #define __TIMER_H
 
-class TimerClass {
-  public:
-    void initialize (void (*callback)(bool start));
-    void loopHandler (void);
-    void tick (void);
-    void secondIncrease (void);
-    void secondDecrease (void);
-    void minuteIncrease (void);
-    void minuteDecrease (void);
-    void displayRefresh (void);
-    void start (void);
-    void stop (void);
-    void resetAlarm (void);
-    void reset (void);
-    
-    volatile bool active = false;
+class TimerClass : public ChronoClass {
+   public:
+    void initialize();
+    void increaseRight(void) volatile;
+    void decreaseRight(void) volatile;
+    void increaseLeft(void) volatile;
+    void decreaseLeft(void) volatile;
+
+    void start(void);
+    void stop(void);
+    void resetAlarm(void);
+    void reset(void);
+
     volatile bool running = false;
     volatile bool alarm = false;
-    NixieDigits_s digits; 
-  
-  private:
-    uint32_t alarmTs = 0;
-    ChronoClass defaultTm;
-    volatile ChronoClass tm;
-    volatile bool tickFlag = false; 
-    void (*callback)(bool) = NULL;
+
+   private:
+    uint32_t alarmTs = 0;  // ToDo: implement timeout for the alarm
+    Timekeeper defaultTm;
+    volatile Timekeeper tm;
 };
