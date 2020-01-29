de20339922af5e4dcefb9efa414f63f18c16bdf2
Peter Nagy <peternagy96@gmail.com>
2020-01-29 08:25:35 +0100

    implement inheritance from chrono

---
+++ code/Timekeeper.cpp
@@ -0,0 +1,37 @@
+
+#include "Timekeeper.h"
+
+void TimekeeperClass::incrementRight(void) volatile {
+    minute++;
+    if (minute > 59) minute = 0, hour++;
+}
+
+void TimekeeperClass::decrementRight(void) volatile {
+    minute--;
+    if (minute < 0) minute = 59, hour--;
+    if (hour < 0) hour = 23;
+}
+
+void TimekeeperClass::incrementLeft(void) volatile {
+    hour++;
+    if (hour > 23) hour = 0;
+}
+
+void TimekeeperClass::decrementLeft(void) volatile {
+    hour--;
+    if (hour < 0) hour = 23, hour--;
+}
+
+void TimekeeperClass::reset(void) volatile {
+    minute = 0;
+    hour = 0;
+}
+
+void TimekeeperClass::copy(volatile ChronoClass *tm) volatile {
+    minute = tm->minute;
+    hour = tm->hour;
+}
+
+void TimekeeperClass::roundup(void) volatile {
+    if (minute > 59) minute = 0, hour++;
+}
---
+++ code/Timekeeper.h
@@ -0,0 +1,21 @@
+/*
+* Class to keep track of time
+*/
+
+#ifndef __TIMEKEEPER_H
+#define __TIMEKEEPER_H
+
+class TimekeeperClass : public Chrono {
+   public:
+    void incrementRight(void) volatile;
+    void decrementRight(void) volatile;
+    void incrementLeft(void) volatile;
+    void decrementLeft(void) volatile;
+
+    void reset(void) volatile;
+    void copy(volatile ChronoClass *) volatile;
+    void roundup(void) volatile;
+
+    volatile int8_t minute = 0;
+    volatile int8_t hour = 0;
+};
