6b41c8e6f6844265ac5f708d21a04c42ec55238d
Peter Nagy <peternagy96@gmail.com>
2020-01-29 08:06:35 +0100

    added set value functions

--- code/Chrono.h
+++ code/Chrono.h
@@ -7,20 +7,10 @@
 
 class ChronoClass {
    public:
-    void incrementDigit1(void) volatile = 0;
-    bool decrementDigit1(void) volatile = 0;
-    void incrementDigit2(void) volatile = 0;
-    bool decrementDigit2(void) volatile = 0;
-    void incrementDigit3(void) volatile = 0;
-    bool decrementDigit3(void) volatile = 0;
-    void incrementDigit4(void) volatile = 0;
-    bool decrementDigit4(void) volatile = 0;
+    void setRight(void) volatile = 0;
+    void setLeft(void) volatile = 0;
 
     void reset(void) volatile = 0;
     void copy(volatile ChronoClass *) volatile = 0;
     void roundup(void) volatile = 0;
-
-    volatile int8_t second = 0;
-    volatile int8_t minute = 0;
-    volatile int8_t hour = 0;
 };
