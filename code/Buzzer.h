f60d2062678d0fcc8ab5c11f8ba731b339683c0e
Peter Nagy <peternagy96@gmail.com>
2020-01-30 11:51:55 +0100

    format

--- code/Buzzer.h
+++ code/Buzzer.h
@@ -5,24 +5,24 @@
  * Buzzer control class
  */
 class BuzzerClass {
-  public:
-    void initialize (uint8_t buzzerPin);
-    void loopHandler (void);
-    void playMelody1 (void);
-    void playMelody2 (void);
-    void stop (void);
+   public:
+    void initialize(uint8_t buzzerPin);
+    void loopHandler(void);
+    void playMelody1(void);
+    void playMelody2(void);
+    void stop(void);
 
     bool active = false;
-    
-  private:
+
+   private:
     bool initialized = false;
     uint8_t buzzerPin;
     uint8_t index = 0;
     uint32_t melodyTs = 0;
     int32_t *melody = melody1;
     //int32_t melody1[9] = { 500, 50, 100, 50, 100, 50, 100, 50, -1 };
-    int32_t melody1[9] = { 1450, 50, 200, 50, 200, 50, -1 };
-    int32_t melody2[3] = { 950, 50, -1 };  
+    int32_t melody1[9] = {1450, 50, 200, 50, 200, 50, -1};
+    int32_t melody2[3] = {950, 50, -1};
 };
 
 /*
